dec52683a9f4cf79174d6ec9357076b1d483cd69
Justin Evans <stupid_nerd99@yahoo.com>
2020-02-03 19:40:43 -0600

    code updates

--- go_practice_2.go
+++ go_practice_2.go
@@ -1,17 +1,22 @@
-//go_practice_1 prints command-line arguments, as well as the command used to invoke file
+//go_practice_2 prints command-line arguments, as well as the command used to invoke file, using string package
 package main
 
 import (
 	//format package for compiling into source files
 	"fmt"
+	//imple functions for string manipulation
+	"strings"
 	//access to operation system
 	"os"
+	//time monitoring
+	"time"
 )
 
 func main() {
-	//loop over arguments by index and value
-	for i, arg := range os.Args {
-		//print both the index element and its value
-		fmt.Println("item", i, "is", arg)
-	}
+	//initializing time measurement
+	start := time.Now()
+	//joining command-line arguments via strings package, passed into empty variable at end
+	fmt.Println(strings.Join(os.Args[1:], " "))
+	//measuring operation runtime
+	fmt.Printf("%.2fs elasped\n", time.Since(start).Seconds())
 }
