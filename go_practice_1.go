ad782fc545e282c03e870cb00a78759a897429a2
Justin Evans <stupid_nerd99@yahoo.com>
2020-02-03 19:49:37 -0600

    reworked-measures runtime, uses explicit variable to loop over, prints both index element and its value

--- go_practice_1.go
+++ go_practice_1.go
@@ -1,4 +1,4 @@
-//go_practice_1 prints command-line arguments
+//go_practice_1 prints command-line arguments, as well as index element and its value, but takes in explicit variable to do so
 package main
 
 import (
@@ -6,15 +6,19 @@ import (
 	"fmt"
 	//access to operation system
 	"os"
+	"time"
 )
 
 func main() {
-	//explicit declaration for variables
-	s, sep := "", ""
-	//_ is used to discard loops index, storing only the element value in arg
-	for _, arg := range os.Args[0:] {
-		s += sep + arg
-		sep = " "
+	//explicit declaration for variable
+	var s = ""
+	//initializing time measurement
+	start := time.Now()
+	//declared both index and value to return both
+	for i, arg := range os.Args[1:] {
+		s := s + arg
+
+		fmt.Println("item", i, "is", s)
 	}
-	fmt.Println(s)
+	fmt.Printf("%.2fs elasped\n", time.Since(start).Seconds())
 }
